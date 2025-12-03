3ad945efd2a606e0ffdabd9670b38df49c9916f7
Lord Drago <clearrio1465@gmail.com>
2025-12-03 14:57:13 +0530

    Add files via upload

---
+++ convering km to mile.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+
+distance_km = float(input("Enter distance in km: "))
+distance_mile = distance_km * 0.621371
+
+print(f"Hi {name}, {distance_km} km is equal to {distance_mile} miles.")
+# This program converts kilometers to miles and greets the user by name.
